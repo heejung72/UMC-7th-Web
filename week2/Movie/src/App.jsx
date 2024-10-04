23c97fe46d8bf3adfcf490888027ab80c7decdc3
heejung <mishel7@naver.com>
2024-10-04 18:57:06 +0900

    Update App.jsx

--- week2/Movie/src/App.jsx
+++ week2/Movie/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         {movies.slice(0, 10).map((movie, index) => (
           <img
             key={index}
-            src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={`Movie ${index + 1}`}
             className="movie-image"
           />
@@ -20,7 +20,7 @@ function App() {
         {movies.slice(10, 20).map((movie, index) => (
           <img
             key={index}
-            src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={`Movie ${index + 11}`}
             className="movie-image"
           />
