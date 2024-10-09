85ab7b8e8a053b3b2098f74489d9ea0f7c7ebd46
heejung <mishel7@naver.com>
2024-10-09 15:00:26 +0900

    Update movies.jsx

--- week3/MovieSite/src/pages/movies.jsx
+++ week3/MovieSite/src/pages/movies.jsx
@@ -1,35 +1,17 @@
-import { useEffect, useState } from 'react'; 
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
 const PageContainer = styled.div`
-  background-color: black;
+  background-color: #black;
   min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  min-width: 130vh;
 `;
-
 const Title = styled.h1`
-  color: white;
-  margin-left: 10px;
-  font-size: 20px;
+  color: white; 
+  margin-left:10px;
+   font-size: 15px; 
 `;
-
-const MovieBoxContainer = styled.div`
-  display: flex;
-  justify-content: space-around;
-  width: 80%;
-`;
-
-const Box = styled.div`
-  width: 200px;
-  height: 100px;
-  margin: 20px;
-  background-color: ${(props) => props.color};
-`;
-
 const NowPlaying = () => {
   const [movies, setMovies] = useState([]);
 
@@ -44,15 +26,14 @@ const NowPlaying = () => {
   }, []);
 
   return (
-    <PageContainer>
-      <Title>현재 상영중인 영화</Title>
-      <MovieBoxContainer>
-        <Box color="red">현재 상영중인</Box>
-        <Box color="yellow">인기있는</Box>
-        <Box color="green">높은 평가를 받은</Box>
-        <Box color="blue">개봉 예정중인</Box>
-      </MovieBoxContainer>
-    </PageContainer>
+    <PageContainer><Title>
+      <h1>현재 상영중인 영화</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+   </Title> </PageContainer>
   );
 };
 
