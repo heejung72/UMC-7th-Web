21b119c8b26659fb4e95c1e9f053f5992e6f83cc
heejung <mishel7@naver.com>
2024-11-20 14:30:22 +0900

    card skeleton 수정

--- week3/MovieSite/src/components/skeleton/card-skeleton.jsx
+++ week3/MovieSite/src/components/skeleton/card-skeleton.jsx
@@ -1,6 +1,5 @@
 import styled, { keyframes } from "styled-components";
 
-
 const skeleton = keyframes`
     0% {
         opacity: 1;
@@ -19,54 +18,65 @@ const skeleton = keyframes`
     }
 `;
 
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(9, 1fr);
+  grid-gap: 15px;
+  padding: 20px;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 `;
 
 const CardMain = styled.div`
-    width: 140px;
-    height: 210px;
-    background-color: rgb(230, 230, 230);
-    border-radius: 10px;
-    overflow: hidden;
-    animation: ${skeleton} 3s 1s infinite linear alternate;
+  width: 140px;
+  height: 210px;
+  background-color: rgb(230, 230, 230);
+  border-radius: 10px;
+  overflow: hidden;
+  animation: ${skeleton} 3s 1s infinite linear alternate;
 `;
 
 const TextWrapper = styled.div`
-    width: 140px;
-    height: 30px;
-    display: flex;
-    flex-direction: column;
-    gap: 2px;
-    margin-top: 5px;
+  width: 140px;
+  height: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+  margin-top: 5px;
 `;
 
 const TitleBox = styled.div`
-    background: rgb(230, 230, 230);
-    height: 14px;
-    border-radius: 5px;
-    animation: ${skeleton} 3s 1s infinite linear alternate;
+  background: rgb(230, 230, 230);
+  height: 14px;
+  border-radius: 5px;
+  animation: ${skeleton} 3s 1s infinite linear alternate;
 `;
 
 const DescriptionBox = styled.div`
-    background: rgb(230, 230, 230);
-    height: 10px;
-    border-radius: 5px;
-    animation: ${skeleton} 3s 1s infinite linear alternate;
+  background: rgb(230, 230, 230);
+  height: 10px;
+  border-radius: 5px;
+  animation: ${skeleton} 3s 1s infinite linear alternate;
 `;
 
-const CardSkeleton = () => {
-    return (
-        <Container>
-            <CardMain />
-            <TextWrapper>
-                <TitleBox />
-                <DescriptionBox />
-            </TextWrapper>
-        </Container>
-    );
+const CardSkeleton = ({ number = 1 }) => {
+  return (
+    <GridContainer>
+      {Array.from({ length: number }).map((_, index) => (
+        <CardContainer key={index}>
+          <CardMain />
+          <TextWrapper>
+            <TitleBox />
+            <DescriptionBox />
+          </TextWrapper>
+        </CardContainer>
+      ))}
+    </GridContainer>
+  );
 };
 
 export default CardSkeleton;
--- week3/MovieSite/src/pages/nowplaying.jsx
+++ week3/MovieSite/src/pages/nowplaying.jsx
@@ -34,6 +34,7 @@ const NowPlaying = () => {
     setCurrentPage(page);
   };
 
+
   if (isLoading) {
     return (
       <div>
